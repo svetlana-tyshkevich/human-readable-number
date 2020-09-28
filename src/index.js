d7ecc71e52ee5155019e28029ae9c6faec410842
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2020-09-29 00:51:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
+    let result = '';
+      const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+      const second_ten = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+      const tens = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+      const num = String(number).split('');
+      const first = num.length - 3;
+      const second = num.length - 2;
+      const third = num.length - 1;
+      if (number === 0 ) return 'zero';
+    
+      if (num[first] ) result += ones[num[first]] + ' hundred ';
+    
+      if  (num[second] === '1') {
+          result +=  second_ten[num[third]];
+      } else {
+          if (num[second] && num[second] !== 0) result += tens[num[second]] ;
+          if (num[third] !== 0) result +=  ones[num[third]] ;
+      }
+      
   
-}
+    return result.trim();
+  }
